c4eea5480789c364cb3fb82bc9df7e7af52f6f07
vladimirgufx <vladimirgufx@gmail.com>
2025-11-23 03:03:58 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 
-# Токен бота от @BotFather (лучше использовать переменные окружения)
-BOT_TOKEN = os.getenv('BOT_TOKEN', '8258538369:AAGarw6bCZ9KW7iR9is6WJPFPxiippkkU08')
+BOT_TOKEN = "8258538369:AAGarw6bCZ9KW7iR9is6WJPFPxiippkkU08"
 
 # Вопросы и варианты ответов
 QUESTIONS = [
@@ -655,5 +654,6 @@ PERFUMES = [
     }
 
 ]
+
 
 
